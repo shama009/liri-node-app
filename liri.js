0ffb50ffa8ccc341960ecfc50b9c8e95d7383ee5
Shama <shamaanjum09@outlook.com>
2017-12-21 20:23:10 -0800

    imdb data

--- liri.js
+++ liri.js
@@ -1,2 +1,46 @@
 // import keys.js to use its contents
 var keys = require("./keys.js");
+var request = require("request");
+
+var userCommand = process.argv[2];
+var userInput = process.argv[3];
+
+if (userCommand === "movie-this") {
+    var queryUrl = "";
+    if(userInput){
+    // Then run a request to the OMDB API with the movie specified
+        queryUrl = "http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy";
+    
+        // This line is just to help us debug against the actual URL.
+        console.log(queryUrl);
+    }
+    else{
+        queryUrl = "http://www.omdbapi.com/?t=Mr.Nobody&y=&plot=short&apikey=trilogy";
+    }
+    request(queryUrl, function (error, response, body) {
+
+        // If the request is successful
+        if (!error && response.statusCode === 200) {
+
+            // Parse the body of the site and recover following
+            // * Title of the movie.
+            // * Year the movie came out.
+            // * IMDB Rating of the movie.
+            // * Rotten Tomatoes Rating of the movie.
+            // * Country where the movie was produced.
+            // * Language of the movie.
+            // * Plot of the movie.
+            // * Actors in the movie.
+
+            console.log("Title: " + JSON.parse(body).Title);
+            console.log("Release Year: " + JSON.parse(body).Year);
+            console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+            console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+            console.log("Country: " + JSON.parse(body).Country);
+            console.log("Language: " + JSON.parse(body).Language);
+            console.log("Plot: " + JSON.parse(body).Plot);
+            console.log("Actors: " + JSON.parse(body).Actors);
+        }
+    });
+}
+
