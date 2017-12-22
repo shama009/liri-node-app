a4af01e48c61cdb3e656daaa408cb37c15191a21
Shama <shamaanjum09@outlook.com>
2017-12-21 19:37:27 -0800

    create keys and run node dependencies

---
+++ keys.js
@@ -0,0 +1,17 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'R0lRE3N70FFgVfL4cDmq5OGt7',
+  consumer_secret: 'OnTq6rKpG7GPlMu4KyTsKnO6mEYUFUPwQhOumVdKxNYjpgSBVN',
+  access_token_key: '943938690497122304-tvgwYW7VfZGUJyrmwvxyMr5pKs7qace',
+  access_token_secret: 'vjUddRgsTMJUhU0I8B5UEplH2bxgJc4a7iKiJWZrrU1hl',
+}
+var spotifyKeys =
+  {
+    client_id: 'e6ae02717d2747aba63cf6d2420cafb8',
+    client_secret: '94eb0e989d974ab4aa02c210a13c390d'
+  };
+module.exports = {
+  twitterKeys: twitterKeys,
+  spotifyKeys: spotifyKeys
+};
---
+++ liri.js
@@ -0,0 +1,2 @@
+// import keys.js to use its contents
+var keys = require("./keys.js");
